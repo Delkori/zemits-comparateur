89d253c949e6e6526d8f96ee85698bc190221cd5
Delkori <271044887+Delkori@users.noreply.github.com>
2026-04-01 13:40:51 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'zemits.fr' },
-      { protocol: 'https', hostname: 'zemits.store' },
-      { protocol: 'https', hostname: 'lteqngcboeiwqjfmcwve.supabase.co' },
-    ],
-  },
-}
-
-module.exports = nextConfig
+    domains: ["zemits.store", "zemits.fr"]
+  }
+};
+module.exports = nextConfig;
