88b3bd8f44d12e2530ccfad4f2e4a23eeaeeda87
Delkori <271044887+Delkori@users.noreply.github.com>
2026-04-01 13:14:58 +0200

    feat: init Next.js project structure with package.json and config

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['zemits.fr', 'zemits.store', 'lteqngcboeiwqjfmcwve.supabase.co'],
+  },
+}
+
+module.exports = nextConfig
